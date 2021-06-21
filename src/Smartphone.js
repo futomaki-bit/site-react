0c5c539a02d1e223b58e9a2f2debd133e287e2b3
futomakiyoin <futomakiyoin@gmail.com>
2021-06-21 14:29:32 -0400

    Edit Smartphone Page

--- src/Smartphone.js
+++ src/Smartphone.js
@@ -7,12 +7,8 @@ const smartphone = (
                     <p>iPhone</p>
                 </div>
                 <div class="column col-sm-6 tallchild ">
-                    <i>
-                        <h3>Wait.</h3>
-                        <h2>What?</h2>
-                    </i>
-                    <p>Learning new things is what a programmer usually does. </p>
-                    <p>Learning iOS now.</p>
+                    <h4>Learning iOS now.</h4>
+                    <br />
                     <div class="seriffont">
                         <h5>"Anyone who stops learning is old, whether at twenty or eighty."</h5>
                         <h6>- Henry Ford</h6>
