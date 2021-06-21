366c720ef345fcf87996802e1b80cd495f1abed7
futomakiyoin <futomakiyoin@gmail.com>
2021-06-21 14:31:07 -0400

    Format Notes.js

--- src/Notes.js
+++ src/Notes.js
@@ -1,31 +1,23 @@
-const notes = ( <
-    div class = "bluetheme parent" >
-    <
-    div class = "container pt-5" >
-    <
-    div class = "row" >
-    <
-    div class = "column col-sm-6 child" >
-    <
-    h3 > < i class = "fas fa-sticky-note" > < /i> <
-    span > < /span>
-    Notes <
-    /h3> <
-    br / >
-    <
-    /div> <
-    div class = "column col-sm-6 tallchild" >
-    <
-    div >
-    <
-    p > In
-    case you missed it, this website follows dark mode according to your device. < /p> <
-    p > Tested with Chrome on Android and Windows, Safari on iOS and macOS.Samsung Internet(browser) doesn 't work.</p> <
-    /div> <
-    /div> <
-    /div> <
-    /div> <
-    /div>
+const notes = (
+    <div class="bluetheme parent">
+        <div class="container pt-5">
+            <div class="row">
+                <div class="column col-sm-6 child">
+                    <h3><i class="fas fa-sticky-note"></i>
+                        <span> </span>
+                        Notes
+                    </h3>
+                    <br />
+                </div>
+                <div class="column col-sm-6 tallchild">
+                    <div>
+                        <p>In case you missed it, this website follows dark mode according to your device.</p>
+                        <p>Tested with Chrome on Android and Windows, Safari on iOS and macOS. Samsung Internet (browser) doesn't work.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 );
 
 export default notes;
