14cbfa385f7bcd3328ee4db97af3f0692c1db4c7
futomakiyoin <72528988+futomaki-bit@users.noreply.github.com>
2022-01-13 09:22:31 -0500

    More username change

--- src/Cover.js
+++ src/Cover.js
@@ -2,7 +2,7 @@ const cover = (<div class="jumbotron cover tealtheme parent">
     <div class="container pt-5">
         <div class="row">
             <div class="column col-sm-12 child">
-                <a href="https://github.com/futomakiyoin">
+                <a href="https://github.com/futomaki-bit">
                     <h2><i class="fab fa-github"></i><span> </span>
                         GitHub
                     </h2>
@@ -10,13 +10,13 @@ const cover = (<div class="jumbotron cover tealtheme parent">
             </div>
             <div class="column col-sm-12 tallchild">
                 <div class="container text-center pt-5">
-                    <a href="https://github.com/futomakiyoin">
+                    <a href="https://github.com/futomaki-bit">
                         <img src="https://avatars.githubusercontent.com/u/72528988?v=4" class="profilepic" alt="futomakiyoin"
                         />
                     </a>
                     <div>
-                        <a href="https://github.com/futomakiyoin">
-                            <h2 class="mt-4">futomakiyoin</h2>
+                        <a href="https://github.com/futomaki-bit">
+                            <h2 class="mt-4">futomaki-bit</h2>
                         </a>
                         <p class="small">Hello, World! Hello, You!</p>
                     </div>
--- src/Github.js
+++ src/Github.js
@@ -3,7 +3,7 @@ const github = (
         <div class="container pt-5">
             <div class="row">
                 <div class="column col-sm-12 child">
-                    <a href="https://github.com/futomakiyoin">
+                    <a href="https://github.com/futomaki-bit">
                         <h3>
                             <i class="fab fa-github"></i><span> </span>
                             GitHub
@@ -12,13 +12,13 @@ const github = (
                 </div>
                 <div class="column col-sm-12 child">
                     <div class="text-center pt-3">
-                        <a href="https://github.com/futomakiyoin">
+                        <a href="https://github.com/futomaki-bit">
                             <img src="https://avatars.githubusercontent.com/u/72528988?v=4" alt="futomakiyoin"
                                 class="profilepic" />
                         </a>
                         <div>
-                            <h2 class="mt-4">futomakiyoin</h2>
-                            <a href="https://github.com/futomakiyoin">
+                            <h2 class="mt-4">futomaki-bit</h2>
+                            <a href="https://github.com/futomaki-bit">
                                 <button type="button" class="btn btn-secondary">View on GitHub</button>
                             </a>
                         </div>
